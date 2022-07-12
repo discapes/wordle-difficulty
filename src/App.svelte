67ec65f3c4d3397d6e8ae5de5e7bd93a3bf83d17
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-07-12 16:12:44 +0300

    more responsive

--- src/App.svelte
+++ src/App.svelte
@@ -18,6 +18,7 @@
   onMount(async () => {
     setTimeout(async () => {
       let response = await fetch(`https://api.datamuse.com/words?sp=${todaysWord}&md=f&max=1`).then((res) => res.json());
+
       freq = +response[0].tags[0].slice(2);
     }, 500);
 
@@ -32,20 +33,22 @@
   });
 </script>
 
-<div class="p-10 pt-24 md:pt-48 h-screen flex flex-col overflow-auto items-center bg-gradient-to-r from-violet-500 to-fuchsia-500">
-  {#if freq}
-    <h1 in:fade={{ duration: 300 }}>
-      Todays wordle appears <span class:visible={showPrevious} in:fade={{ duration: 700 }} class="text-[cyan]">{freq.toFixed(2)}</span> times in a million
-      words.
-      <br />
-      {#if showPrevious}
-        <span in:fade={{ duration: 500 }}>Previous:</span>
-      {/if}
-    </h1>
-  {/if}
-  {#each otherFreqs as freq, i}
-    <h2 in:fly={{ y: 200, duration: 500 }}>{otherWords[i]}: {freq.toFixed(2)}</h2>
-  {/each}
+<div class="h-screen flex flex-col overflow-hidden items-center justify-center bg-gradient-to-r from-violet-500 to-fuchsia-500">
+  <div class="min-h-[630px] md:pt-10 sm:max-w-[80vw] flex flex-col items-center px-10">
+    {#if freq}
+      <h1 in:fade={{ duration: 300 }}>
+        Todays wordle appears <span class:visible={showPrevious} in:fade={{ duration: 700 }} class="text-[cyan]">{freq.toFixed(2)}</span> times in a million
+        words.
+        <br />
+        {#if showPrevious}
+          <span in:fade={{ duration: 500 }}>Previous:</span>
+        {/if}
+      </h1>
+    {/if}
+    {#each otherFreqs as freq, i}
+      <h2 in:fly={{ y: 200, duration: 500 }}>{otherWords[i]}: {freq.toFixed(2)}</h2>
+    {/each}
+  </div>
 </div>
 
 <style global lang="postcss">
@@ -63,7 +66,7 @@
     display: block;
     font-weight: bold;
     font-size: 2em;
-    margin-block-start: 0.67em;
+    /* margin-block-start: 0.67em; */
     margin-block-end: 0.67em;
   }
 
